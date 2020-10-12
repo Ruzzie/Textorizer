fb39e95a2c081ba04c1b522b7ffcef1eb82fbcc2
Ruzzie <dorus.verhoeckx@gmail.com>
2020-10-12 09:31:09 +0200

    [impr] moved html test input file to testdata directory; added team shared recognized words

--- source/Textorizer.UnitTests/HtmlTextorizerTests.cs
+++ source/Textorizer.UnitTests/HtmlTextorizerTests.cs
@@ -24,47 +24,41 @@ namespace Textorizer.UnitTests
         [TestCase("4. a<a<a", "4. a<a<a")]
         [TestCase("5. aaa<a<p>", "5. aaa<a\n")]
         [TestCase("aaa<1<p>", "aaa<1\n")]
-
         [TestCase("pre <br>post", "pre \npost")]
         [TestCase("pre <br/>post", "pre \npost")]
         [TestCase("pre <br>post</br>", "pre \npost")]
         [TestCase("pre <hr>post", "pre \npost")]
         [TestCase("pre&nbsp;post", "pre post")]
         [TestCase("pre&#160;post", "pre post")]
-
         [TestCase("A this is no html: 1 < 3 and 1 > 3", "A this is no html: 1 < 3 and 1 > 3")]
         [TestCase("B this is html: <p> 1 < 3 and 1 > 3</p>", "B this is html: \n1 < 3 and 1 > 3\n")]
         [TestCase("C this is html: <p   > 1 < 3 and 1 > 3</p>", "C this is html: \n1 < 3 and 1 > 3\n")]
-
         [TestCase("1.<p class=\"c n\\<\">a</p>", "1.\na\n")]
         [TestCase("2.<p class=\"c n\\<\">b<br/>a</p>", "2.\nb\na\n")]
-        [TestCase("1. hoi<script>var x = '<>';window.close();</script>","1. hoi")]
-        [TestCase("2. hoi<script>var x = '\"<>';window.close();</script>","2. hoi")]
-        [TestCase("3. hoi<script>var x = \"'<>'\";window.close();</script>","3. hoi")]
-        [TestCase("4. hoi<script>var x = \"'<>'\";window.close();</s>","4. hoi")]
-        [TestCase("5. hoi<script>var x = \" '<>' \";window.close();<p>a<p/>","5. hoi\na\n")]
-        [TestCase("1. hoi<style>#\\~\\!\\@\\$\\%\\^\\&\\*\\(\\)\\_\\+-\\=\\,\\.\\/\\'\\;\\:\\\"\\?\\>\\<\\[\\]\\\\\\{\\}\\|\\`\\#{</style><p>a</p>","1. hoi\na\n")]
-        [TestCase("2. hoi<style>.c{'a:1'};</style><p>a</p>","2. hoi\na\n")]
-
-        [TestCase("1. X<b>-bld</b>","1. X-bld")]
-
-        [TestCase("1. <p>Hello</p>    How are you? ","1. \nHello\nHow are you? ")]
-        [TestCase("li 1. <li>Y   </li>","li 1. \t- Y \n")]
-        [TestCase("li 2. <li>   X</li>","li 2. \t- X\n")]
-        [TestCase("li 3. <li>X <b>bld</bld></li>","li 3. \t- X bld\n")]
-
-        [TestCase("1. <b>&lt;word&gt;</b>","1. <word>")]
-        [TestCase("2. <b>&ltword&gt</b>","2. &ltword&gt")]
-
-        [TestCase("1. <p>hi</p>\r\n\r\n\r\n        ok?","1. \nhi\nok?")]
-        [TestCase("2. <b>hi</b>\r\n\r\n\r\n        ok?","2. hi ok?")]
-        [TestCase("3. <div><b>hi</b>\r\n\r\n\r\n        ok?</div>","3. hi ok?")]
-        [TestCase("4. <div>a <a>hi</a>  ok?</div>","4. a hi ok?")]
-
-
-        [TestCase("pre 1. <pre>\n code \r\n\r\n\r\n        ok?</pre>","pre 1.  code \r\n\r\n\r\n        ok?")]
-        [TestCase("pre 2. <pre>\n <span>code \r\n\r\n\r\n</span>        ok?</pre>","pre 2.  code \r\n\r\n\r\n        ok?")]
-        [TestCase("pre 3. <pre>\n<pre> <span>code \r\n\r\n\r\n</span></pre>        ok?</pre>","pre 3.  code \r\n\r\n\r\n        ok?")]
+        [TestCase("1. hoi<script>var x = '<>';window.close();</script>", "1. hoi")]
+        [TestCase("2. hoi<script>var x = '\"<>';window.close();</script>", "2. hoi")]
+        [TestCase("3. hoi<script>var x = \"'<>'\";window.close();</script>", "3. hoi")]
+        [TestCase("4. hoi<script>var x = \"'<>'\";window.close();</s>", "4. hoi")]
+        [TestCase("5. hoi<script>var x = \" '<>' \";window.close();<p>a<p/>", "5. hoi\na\n")]
+        [TestCase("1. hoi<style>#\\~\\!\\@\\$\\%\\^\\&\\*\\(\\)\\_\\+-\\=\\,\\.\\/\\'\\;\\:\\\"\\?\\>\\<\\[\\]\\\\\\{\\}\\|\\`\\#{</style><p>a</p>",
+                  "1. hoi\na\n")]
+        [TestCase("2. hoi<style>.c{'a:1'};</style><p>a</p>", "2. hoi\na\n")]
+        [TestCase("1. X<b>-bld</b>", "1. X-bld")]
+        [TestCase("1. <p>Hello</p>    How are you? ", "1. \nHello\nHow are you? ")]
+        [TestCase("li 1. <li>Y   </li>", "li 1. \t- Y \n")]
+        [TestCase("li 2. <li>   X</li>", "li 2. \t- X\n")]
+        [TestCase("li 3. <li>X <b>bld</bld></li>", "li 3. \t- X bld\n")]
+        [TestCase("1. <b>&lt;word&gt;</b>", "1. <word>")]
+        [TestCase("2. <b>&ltword&gt</b>", "2. &ltword&gt")]
+        [TestCase("1. <p>hi</p>\r\n\r\n\r\n        ok?", "1. \nhi\nok?")]
+        [TestCase("2. <b>hi</b>\r\n\r\n\r\n        ok?", "2. hi ok?")]
+        [TestCase("3. <div><b>hi</b>\r\n\r\n\r\n        ok?</div>", "3. hi ok?")]
+        [TestCase("4. <div>a <a>hi</a>  ok?</div>", "4. a hi ok?")]
+        [TestCase("pre 1. <pre>\n code \r\n\r\n\r\n        ok?</pre>", "pre 1.  code \r\n\r\n\r\n        ok?")]
+        [TestCase("pre 2. <pre>\n <span>code \r\n\r\n\r\n</span>        ok?</pre>",
+                  "pre 2.  code \r\n\r\n\r\n        ok?")]
+        [TestCase("pre 3. <pre>\n<pre> <span>code \r\n\r\n\r\n</span></pre>        ok?</pre>",
+                  "pre 3.  code \r\n\r\n\r\n        ok?")]
         public void SmokeTests(string htmlInput, string expectedOutput)
         {
             //Act
@@ -85,11 +79,12 @@ namespace Textorizer.UnitTests
         public void HtmlFileShouldGiveSomeReasonableResult()
         {
             //Arrange
-            var testFilename = Path.Join(TestContext.CurrentContext.TestDirectory, "input_html_01.html");
+            var testFilename = Path.Join(TestContext.CurrentContext.TestDirectory, "testdata", "input_html_01.html");
 
             //Act
             var result = Textorize.HtmlToPlainText(File.ReadAllText(testFilename));
-            File.WriteAllText(Path.Join(TestContext.CurrentContext.TestDirectory, "input_html_01-sanitized.txt" ),result);
+            File.WriteAllText(Path.Join(TestContext.CurrentContext.TestDirectory, "input_html_01-sanitized.txt"),
+                              result);
 
             //Assert
             Console.Write(result);
